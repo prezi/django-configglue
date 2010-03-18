18005e301be15159989ae3736d0cbb1c28beb025
Ricardo Kirkner <ricardo.kirkner@canonical.com>
2010-03-18 16:34:10 -0300

    refactored to support only retrieving settings

--- django_settings/management/commands/settings.py
+++ django_settings/management/commands/settings.py
@@ -1,3 +1,4 @@
+import sys
 from optparse import make_option
 
 from django.conf import settings, global_settings
@@ -9,44 +10,26 @@ class Command(BaseCommand):
         make_option('--global', action='store_true',
             dest='include_global',
             help='include global settings in lookup'),
+        make_option('--show', action='store_true', dest='show_current',
+            help='show current settings'),
         )
-    help = """
-    Read and write configuration options.
-
-    command can be show, get or set
-    """
-    args = 'command [arg1 ...]'
+    help = 'Show settings'
+    args = '[setting ...]'
 
     def handle(self, *args, **options):
-        action = options.get('action')
+        show_current = options.get('show_current')
         include_global = options.get('include_global')
-        setting = options.get('setting')
-        value = options.get('value')
 
-        num_args = len(args)
-        if action is None and num_args > 0:
-            action = args[0]
-        if setting is None and num_args > 1:
-            setting = args[1].upper()
-        if value is None and num_args > 2:
-            value = args[2]
+        if not args and not show_current:
+            self.print_help(sys.argv[0], sys.argv[1])
+            return
 
-        if action == 'show':
+        if args:
+            settings = map(str.upper, args)
+            self.get(settings)
+
+        if show_current:
             self.show(include_global)
-        elif action == 'get':
-            if setting is None:
-                msg = 'GET command requires a setting name as argument'
-                raise CommandError(msg)
-            self.get(setting)
-        elif action == 'set':
-            if value is None:
-                msg = 'SET command requires a setting name and a value ' \
-                      'as arguments'
-                raise CommandError(msg)
-            self.set(setting, value)
-        else:
-            msg = "There is no such command: --%s" % action
-            raise CommandError(msg)
 
     def show(self, include_global=False):
         global_settings_ = dir(global_settings)
@@ -58,19 +41,16 @@ class Command(BaseCommand):
         else:
             local_settings = (s for s in all_settings if s not in global_settings_)
         for name in local_settings:
-            lines.append("%s = %s" % (name, repr(getattr(settings, name))))
+            lines.append("%s = %r" % (name, getattr(settings, name)))
 
         print '\n'.join(lines)
 
-    def get(self, setting):
-        marker = object()
-        value = getattr(settings, setting, marker)
-        if value is marker:
-            print "Setting '%s' not found" % setting
-        else:
-            print "%s = %s" % (setting, value)
+    def get(self, keys):
+        __marker = object()
+        for key in keys:
+            value = getattr(settings, key, __marker)
+            if value is __marker:
+                print "setting '%s' not found" % key
+            else:
+                print "%s = %r" % (key, value)
 
-    def set(self, setting, value):
-        old_value = getattr(settings, setting)
-        setattr(settings, setting, value)
-        print "%s = %s (was '%s')" % (setting, value, old_value)
