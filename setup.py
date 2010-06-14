8eb5f1e38733389183c2db818bbe20dfd7978add
Ricardo Kirkner <ricardo.kirkner@canonical.com>
2010-06-14 18:25:49 -0300

    bumped the version number due to adding django 1.1 support

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-settings',
-    version='0.1',
+    version='0.2',
     author='Canonical ISD Hackers',
     author_email='canonical-isd@lists.launchpad.net',
     license='Proprietary',
