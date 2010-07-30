0496341709a1ac4f63bc1fb324a1bc7a1817d32d
Anthony Lenton <anthony.lenton@canonical.com>
2010-07-30 17:18:56 -0300

    Removed a conflict.

--- django_schemaconfig/utils.py
+++ django_schemaconfig/utils.py
@@ -27,26 +27,3 @@ def update_settings(parser, env):
     # import config into settings module
     settings = get_django_settings(parser)
     env.update(settings)
-<<<<<<< TREE
-
-# This function is based on code from the Django project.
-# Please see the license file in the third-party/django directory
-def execute_manager(settings, argv=None):
-    parser = LaxOptionParser(usage="%prog subcommand [options] [args]",
-                             version=get_version(),
-                             option_list=BaseCommand.option_list)
-    try:
-        parser, options, args = schemaconfigglue(settings.parser, op=parser)
-        update_settings(settings.parser, vars(settings))
-        argv = [sys.argv[0]] + args
-    except Exception:
-        # capture errors to allow the manager to continue
-        # they can be shown using django_settings'
-        # settings --validate command
-        argv = argv if argv is not None else sys.argv
-
-    setup_environ(settings)
-    utility = ManagementUtility(argv)
-    utility.execute()
-=======
->>>>>>> MERGE-SOURCE
