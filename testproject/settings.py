cf3d373eda6826e6f6254d2c3e6c4fa39e9d57db
Anthony Lenton <anthony.lenton@canonical.com>
2010-08-02 12:29:33 -0300

    Put settings.py back where it used to be, and symlinked it in from testproject.

--- testproject/settings.py
+++ testproject/settings.py
@@ -1,18 +1,1 @@
-# Copyright 2010 Canonical Ltd.  This software is licensed under the
-# GNU Lesser General Public License version 3 (see the file LICENSE).
-
-import django
-from schemaconfig import SchemaConfigParser
-from django_schemaconfig.utils import update_settings
-from django_schemaconfig.schema import schemas
-
-
-DjangoSchema = schemas.get(django.get_version())
-# parse config file
-parser = SchemaConfigParser(DjangoSchema())
-parser.read(['main.cfg', 'test.cfg'])
-update_settings(parser, locals())
-
-# keep parser reference
-__SCHEMACONFIGPARSER__ = parser
-
+../django_schemaconfig/tests/settings.py
